53de6f586ecee88e31a96e8a3c35b3b5db4ce03b
Dmitrii Neverov <dmi.neverov@gmail.com>
2020-09-21 19:54:26 +1000

    refactor tests

--- test/test_nmax.rb
+++ test/test_nmax.rb
@@ -1,28 +1,42 @@
 require 'minitest/autorun'
 require_relative '../lib/nmax.rb'
 
-class TestNmax < Minitest::Test
-  def test_io_without_numbers
+module TestNmaxHelper
+  def start(strings, argv)
     io = StringIO.new
-    io.puts 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...'
+    strings.each { |str| io.puts str}
     io.rewind
     $stdin = io
-    ARGV.replace ['1']
-    out, _err = capture_io do
+    ARGV.replace argv
+    @out, _err = capture_io do
       Nmax.run
     end
+  end
+end
+
+class TestNmax < Minitest::Test
+  include TestNmaxHelper
+  attr_reader :out
+  def test_without_numbers
+    strings = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit...']
+    argv = ['1']
+    start(strings, argv)
     assert_equal("\n", out)
   end
 
-  def test_io_with_one_number
-    io = StringIO.new
-    io.puts 'one1one'
-    io.rewind
-    $stdin = io
-    ARGV.replace ['1']
-    out, _err = capture_io do
-      Nmax.run
-    end
+  def test_with_one_number
+    strings = ['one1one']
+    argv = ['1']
+    start(strings, argv)
     assert_equal("1\n", out)
+  end
+
+  def test_largest_numbers
+    strings = ['L 1 ipsum 4 consectetur..1000',
+               'Lorem 2 3000dolor am 3',
+               'tetur 5 2000 elit']
+    argv = ['3']
+    start(strings, argv)
+    assert_equal("3000\n2000\n1000\n", out)
   end
 end
